a5d77867d0a962c5b85df30a942fbfcfd9204099
Valery Dubrava <valery.dubrava@gmail.com>
2018-02-07 23:54:57 +0300

    update gas pre/post invoke balance

--- contracts/JouleConsts.sol
+++ contracts/JouleConsts.sol
@@ -4,13 +4,13 @@ contract usingConsts {
     uint constant GWEI = 0.001 szabo;
     // this two values influence to the reward price! do not change for already registered contracts!
     // remaining gas - amount of gas to finish transaction after invoke
-    uint constant REMAINING_GAS = 10000;
+    uint constant REMAINING_GAS = 15000;
     // idle gas - gas to joule (including proxy and others) invocation, excluding contract gas
-    uint constant IDLE_GAS = REMAINING_GAS + 30000;
+    uint constant IDLE_GAS = REMAINING_GAS + 25000;
     // not, it mist be less then 0x00ffffff, because high bytes might be used for storing flags
     uint constant MAX_GAS = 4000000;
     // Code version
-    bytes8 constant VERSION = 0x0107005a06d3827c;
+    bytes8 constant VERSION = 0x0107005b0f445394;
     //                          ^^ - major
     //                            ^^ - minor
     //                              ^^^^ - build
